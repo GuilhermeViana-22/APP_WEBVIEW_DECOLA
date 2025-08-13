6c1d45fad123175b708d5829cec8ee038e22344d
William <William@ZEUS>
2025-08-12 23:49:40 -0300

    feat: ajustes nova api e correção form login.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -52,14 +52,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b51c938d2f12df02bce76003e6fc001f
 "favicon.ico": "a487c53ce4158c2cdbe76ae7dd1ffa9d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ffef7a52172e2fb8e170a70abaf849b9",
+"flutter_bootstrap.js": "fb0fe096903706c64d9971898ce22714",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "543e87a26d7d125ebaef79989ed9f806",
 "/": "543e87a26d7d125ebaef79989ed9f806",
-"main.dart.js": "8918ca6edd4819456cd68123d92d5d4d",
+"main.dart.js": "e28a1eff379d6c601f2bf30280e72614",
 "manifest.json": "00af51f9516b3ba7bb3e4c29704109eb",
 "version.json": "fe31f9bcbcd85c77b6fe0c8666142cbb"};
 // The application shell files that are downloaded before a service worker can
